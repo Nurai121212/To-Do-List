97f8901dc5a329b6b697ce93c70b91ab737a5e63
Nurai <nooraselfeght@gmail.com>
2022-03-27 22:00:23 +0600

    added notification

--- js/index.js
+++ js/index.js
@@ -1,18 +1,31 @@
 let taskList = $('.task-list');
 let taskInput = $('.task-input');
+let notification = $('.notification');
+
 
 $(document).ready(function(){
+  function displayNotification(){
+    if(!taskList.children().length){
+      notification.fadeIn('fast');
+    }else{
+      notification.css('display','none');
+    }
+  };
   $('.task-add').on('click', function(e){
     if(!taskInput.val()){return false}
     taskList.append(`<li><p class="task-text"> ${taskInput.val()}</p><button class="delete">delete</button></li>`);
     taskInput.val('');
+    displayNotification();
     $('.footer').removeClass("focus");
 
     $('.delete').on('click', function(){
       let parent = $(this).parent();
-      parent.fadeOut('fast');
+      parent.fadeOut('fast').remove();
+
+      displayNotification();
     })
   });
+
   taskInput.focus(function() {
     $('.footer').addClass("focus");
   })
